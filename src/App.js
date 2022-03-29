d214cbab29b3931cff6240d56cad372a363caff3
asadullah00alimran <asadullahalimran002gmail.com>
2022-03-30 03:23:38 +0600

    add bg image and heading text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 function App() {
   return (
+    <>
     <Navbar />
+    <Header />
+    
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class = "header-wraper">
+        <div class = "main-info">
+            <h1>Web Development | Software Engineering</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
